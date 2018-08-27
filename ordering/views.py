874594b23c3bfe78d8b107ef506b4f8bb370351c
Jethro Muller <git@jethromuller.co.za>
2018-08-28 00:10:28 +0200

    Accept url query params in index view
    Rename context var hidden_list to hidden_show_list

--- ordering/views.py
+++ ordering/views.py
@@ -1,17 +1,25 @@
-from flask import render_template
+from flask import render_template, request
 
 from . import app
-from .utils import get_full_series_episode_list
+from .utils import _get_bool, get_full_series_episode_list
 
 
 @app.route('/', methods=['GET'])
 def index():
     context = {}
 
-    episode_list = get_full_series_episode_list()
+    newest_first = request.args.get('newest_first', default=False, type=_get_bool)
+    hide_shows_list = request.args.getlist('hide_show')
+
+    episode_list = get_full_series_episode_list(excluded_series=hide_shows_list)
+
+    if newest_first:
+        episode_list = episode_list[::-1]
 
     context['table_content'] = episode_list
     context['show_list'] = app.config['SHOW_DICT']
+    context['hidden_show_list'] = hide_shows_list
+    context['newest_first'] = newest_first
 
     return render_template('index.html', **context)
 
