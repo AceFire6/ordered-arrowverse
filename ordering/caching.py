dabb9b409069212f822f339301f9173673eeb164
Jethro Muller <git@jethromuller.co.za>
2021-01-25 12:58:43 +0200

    Add & use orjson to handle more caching

--- ordering/caching.py
+++ ordering/caching.py
@@ -1,7 +1,7 @@
 from functools import wraps
-import json
 from logging import getLogger
 
+import orjson
 from redis import Redis
 
 from ordering.settings import REDIS_URL
@@ -11,16 +11,26 @@ cache = Redis.from_url(REDIS_URL)
 logger = getLogger(__name__)
 
 
+def serialized_response(response):
+    def handle_bytes(value):
+        if isinstance(value, bytes):
+            return value.decode('utf-8')
+
+        raise TypeError
+
+    return orjson.dumps(response, default=handle_bytes)
+
+
 def safe_cache_content(timeout=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            hashed_input = hash(f'{args}-{kwargs})')
-            cache_key = f'{func.__name__}-{hashed_input}'
+            inputs = f'{args}-{kwargs}'
+            cache_key = f'{func.__name__}-{inputs}'
 
             cached_value = cache.get(cache_key)
             if cached_value:
-                return json.loads(cached_value.decode('utf-8'))
+                return orjson.loads(cached_value.decode('utf-8'))
 
             try:
                 response = func(*args, **kwargs)
@@ -32,9 +42,9 @@ def safe_cache_content(timeout=None):
                     logger.error('No valid cached LTS response - returning error response')
                     raise exception
 
-                return json.loads(cached_json_lts_response.decode('utf-8'))
+                return orjson.loads(cached_json_lts_response.decode('utf-8'))
             else:
-                json_response = json.dumps(response)
+                json_response = serialized_response(response)
                 cache.set(cache_key, json_response, ex=timeout)
                 cache.set(f'{cache_key}-lts', json_response)
 
