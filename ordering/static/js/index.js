f19d56eb352ddc66aac3af19ca028f984100993b
Michel Albert <michel@albert.lu>
2021-05-24 10:09:03 +0200

    Add checkbox to mark "watched" episodes
    Signed-off-by: Michel Albert <michel@albert.lu>

--- ordering/static/js/index.js
+++ ordering/static/js/index.js
@@ -1,6 +1,13 @@
 (function($, Cookies) {
     'use strict';
 
+    /**
+     * key-names for local-storage data
+     */
+    let localStorageKeys = {
+        WATCHED_EPISODES: "watchedEpisodes",
+    };
+
     var disableColours = function() {
         $('.episode, thead').addClass('no-color');
         $('#episode-list').addClass('table-striped table-hover');
@@ -56,7 +63,63 @@
         }
     };
 
+    /**
+     * Convert the "data-series" and "data-episode-id" into a sinlge key for
+     * LocalStorage.
+     * 
+     * @param  {Element} element the element which contains the data attributes.
+     * @returns A string identifying the episode
+     */
+    var getLSEpisodeKey = function (element) {
+        let series = element.attributes["data-series"].value;
+        let episode = element.attributes["data-episode-id"].value;
+        let key = `${series}-${episode}`;
+        return key;
+    }
+
+    /**
+     * Updates localStorage with "watched" information of an episode
+     * 
+     * 
+     * @param {ChangeEvent} evt The event 
+     */
+    var updateWatched = function (evt) {
+        let newValue = evt.target.checked;
+        let key = getLSEpisodeKey(evt.target);
+        let watchedEpisodes = JSON.parse(
+            localStorage.getItem(localStorageKeys.WATCHED_EPISODES) || "[]"
+        );
+        let index = watchedEpisodes.indexOf(key)
+        if (newValue === true && index === -1) {
+            watchedEpisodes.push(key);
+        } else if (newValue === false && index !== -1) {
+            watchedEpisodes.splice(index, 1);
+        }
+        localStorage.setItem(
+            localStorageKeys.WATCHED_EPISODES, JSON.stringify(watchedEpisodes)
+        );
+        setWatchedDisplayState();
+    };
+
+    /**
+     * Hide/Show episodes, according to the "watched" state
+     */
+    var setWatchedDisplayState = function () {
+        let watchedEpisodes = JSON.parse(
+            localStorage.getItem(localStorageKeys.WATCHED_EPISODES) || "[]"
+        );
+        $('.episode').map(function() {
+            let key = getLSEpisodeKey(this);
+            if (watchedEpisodes.includes(key)) {
+                this.style.display = 'none';
+            } else {
+                this.style.display = 'tableRow';
+            }
+        });
+    };
+
     var registerListeners = function() {
+        $('.watchedToggle').change(updateWatched);
         $('#no-color').click(function() {
             if (Cookies.get('colour') === '1') {
                 disableColours();
@@ -91,6 +154,7 @@
           width: '100%',
         });
         registerListeners();
+        setWatchedDisplayState();
 
         var colourSetting = Cookies.get('colour');
         if (colourSetting === undefined) {
