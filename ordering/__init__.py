41829cf92b7ee478a6df424c229eb6c8bba4249d
Jethro Muller <jethro@flickswitch.co.za>
2021-08-17 13:26:56 +0200

    Remove unused flask-assets

--- ordering/__init__.py
+++ ordering/__init__.py
@@ -1,9 +1,7 @@
-from flask_assets import Environment
 from quart import Quart
 from quart_compress import Compress
 from quart_minify import Minify
 # from tortoise.contrib.quart import register_tortoise
-from webassets import Bundle
 
 from .settings import DATABASE_URL, DEBUG
 from .url_converters import ListConverter
@@ -11,13 +9,6 @@ from .url_converters import ListConverter
 app = Quart(__name__)
 
 app.config.from_pyfile('settings.py')
-
-js_assets = Bundle('js/cookie.js', 'js/index.js', filters='rjsmin', output='gen/bundled.js')
-css_assets = Bundle('css/index.css', filters='cssmin', output='gen/bundled.css')
-
-assets = Environment(app)
-assets.register('js_all', js_assets)
-assets.register('css_all', css_assets)
 
 # Minify HTML and any inline JS or CSS
 Minify(app, js=True)
