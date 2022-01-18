919d2df43e71c97f7bfa548c4d9efdc61ade59dc
Jethro Muller <jethro@stitch.money>
2022-01-18 23:36:23 +0200

    Use https for fandom root url

--- ordering/settings.py
+++ ordering/settings.py
@@ -43,7 +43,7 @@ SUPERGIRL_URL = 'List_of_Supergirl_episodes'
 SUPERMAN_AND_LOIS_URL = 'List_of_Superman_%26_Lois_episodes'
 VIXEN_URL = 'List_of_Vixen_episodes'
 
-FANDOM_ROOT = 'http://arrow.fandom.com/wiki/'
+FANDOM_ROOT = 'https://arrow.fandom.com/wiki/'
 WIKIPEDIA_ROOT = 'https://en.wikipedia.org/wiki/'
 
 DATABASE_URL = env('DATABASE_URL', default='postgres://localhost:5432/arrowverse_db')
